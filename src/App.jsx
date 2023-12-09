82a5dbd748804ecafe61ff08b6f6975db4ed2930
“firstname <valid-email>
2023-12-09 23:14:04 +0530

    add all changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Aman Munjekar",
+  title: "Java Programmer and Web Developer",
+  email: "aman.munjekar@studentambassadors.com",
+  gitHub: "amanmunjekar",
+  instagram: "amanmunjekar",
+  linkedIn: "amanmunjekar",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,27 +16,27 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/wood.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "wood  background";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a student of Master of Computer Application. I am pursuing my graduation in Nagpur Univercity";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
+  "Web  Developer",
+  "Java Programmer",
+  "Coder",
   "Focus group testing",
   "Mobile user interfaces",
-  "Graphic design",
+  
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "This is my Portfolio website.";
 
 const About = () => {
   return (
